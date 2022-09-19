cb4a8d028759933057e8b62039a3891c6c2b8314
JamesA <97204146+jamesafonja@users.noreply.github.com>
2022-09-19 11:14:58 -0500

    Initial Commit

---
+++ JSONParsing/JSONParsingApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONParsingApp.swift
+//  JSONParsing
+//
+//  Created by Revive on 9/19/22.
+//
+
+import SwiftUI
+
+@main
+struct JSONParsingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
